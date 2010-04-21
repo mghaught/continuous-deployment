1a20811b4e3953e23df60257f6156bab0747f2a0
Andrew Kappen <akappen@gmail.com>
2010-04-21 14:14:57 -0500

    Cleaned up form, logging, and system output to tmp.

--- integrity/deploy.rb
+++ integrity/deploy.rb
@@ -7,28 +7,26 @@ module Integrity
       def self.to_haml
         <<-haml
 %p.normal
-  %label{ :for => "deploy_notifier_deploy" } Continuous Deployment Command
+  %label{ :for => "deploy_notifier_deploy" } Deploy Script
   %input.text#deploy_notifier_deploy{ :name => "notifiers[Deploy][deploy]", :type => "text", :value => config["deploy"] }
         haml
       end
 
-      def to_s
-        "Deploy"
-      end
-
       def initialize(build, config={})
         @deploy = config.delete("deploy")
-        Integrity.log("Initiatizing Deploy notifier for build: #{build.commit.identifier}, current status is #{build.status}, command is #{@deploy}")
+        log("Initiatizing... deploy command is '#{@deploy}'")
         super
       end
 
       def deliver!
-        Integrity.log("\n\n#{full_message}\n\n")
-        message = "Build status: #{build.status} at #{Time.now}"
-        Integrity.log("Deploy notifier - DELIVER message: #{message}")
-        system("echo '#{message}' >> /tmp/worked")
-        Integrity.log("Build FAILED") if build.failed?
-        Integrity.log("Build SUCCESS") if build.successful?
+        log("Full build message:\n#{full_message}")
+        system("echo '#{build.status} - #{build.commit.identifier}' >> /tmp/deploy_notifier")
+        log("FAILED") if build.failed?
+        log("SUCCESS") if build.successful?
+      end
+
+      def log(msg)
+        Integrity.log('Deploy') {msg}
       end
     end
 
